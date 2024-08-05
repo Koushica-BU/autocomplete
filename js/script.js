62ed6a74bd09434b975e04bc3faed8201eb1e3e5
Koushica-BU <koushica01@gmail.com>
2024-08-05 15:54:52 -0500

    update script.js
    1) highlighting search term
    2) added debounce

--- js/script.js
+++ js/script.js
@@ -1,47 +1,77 @@
-let html;
-let searchUsers = (id,e) => {
-    let Nstarts = [], NContains = [], SContains = [];
-    html = "";
-	let searchkey = id.value.trim(" ").toLowerCase();
-    if ( searchkey === ""){
-		document.getElementById("result").innerText = "";
-	}
-    else{
-        for ( let index = 0; index < userData.length; index++ ) {
-            let secondary = 
-            userData[index].email.toLowerCase().indexOf(searchkey) > -1 ? userData[index].email :
-            userData[index].company.toLowerCase().indexOf(searchkey) > -1 ? userData[index].company :
-            userData[index].address.toLowerCase().indexOf(searchkey) > -1 ? userData[index].address : 
-            userData[index].phone.toLowerCase().indexOf(searchkey) > -1 ? userData[index].phone : '';
+let debounceTimeout;
+const debounceDelay = 300;
 
-            let IsName = userData[index].displayName.toLowerCase().indexOf(searchkey);
-            let user = {
-                primary: userData[index].displayName,
-                secondary: secondary || userData[index].company
-            }
-            if(IsName > -1) {
-                IsName == 0 ? Nstarts.push(user) : NContains.push(user);
-            }
-            else if(secondary){
-                SContains.push(user);
-            }            
+const debounce = (func, delay) => {
+    return function (...args) {
+        clearTimeout(debounceTimeout);
+        debounceTimeout = setTimeout(() => func.apply(this, args), delay);
+    };
+};
+
+const searchUsers = debounce((input) => {
+    const searchkey = input.value.trim().toLowerCase();
+    clearResults();
+
+    if (searchkey === "") return;
+
+    const { Nstarts, NContains, SContains } = categorizeUsers(searchkey);
+    const combinedResults = [...Nstarts, ...NContains, ...SContains];
+    publishUsers(combinedResults);
+}, debounceDelay);
+
+const categorizeUsers = (searchkey) => {
+    const Nstarts = [], NContains = [], SContains = [];
+
+    userData.forEach(user => {
+        const secondary = findSecondaryMatch(user, searchkey);
+        const IsName = user.displayName.toLowerCase().indexOf(searchkey);
+
+        const userObj = {
+            primary: highlightMatched(user.displayName, searchkey),
+            secondary: highlightMatched(secondary || user.company, searchkey)
+        };
+
+        if (IsName > -1) {
+            IsName === 0 ? Nstarts.push(userObj) : NContains.push(userObj);
+        } else if (secondary) {
+            SContains.push(userObj);
         }
-        publishUsers(Nstarts.concat(NContains,SContains),searchkey)
-    }
-}
+    });
 
-let publishUsers = (users,searchkey) => {
-    let result = document.getElementById("result");
-    let len = searchkey.length;
-    let matched = '';
-    for ( index = 0; index < users.length ; index++){
-        html += `<div class='kbu-user-wrap'>
-                    <div class='userimg'><img src='http://placehold.it/32x32'></div>
-                    <div class='user-desc'> 
-                        <span>${users[index].primary}</span>
-                        <p>${users[index].secondary}<p>
-                    </div>
-                </div>`;
-    }
-    result.innerHTML = html ;
-}
+    return { Nstarts, NContains, SContains };
+};
+
+const findSecondaryMatch = (user, searchkey) => {
+    const { email, company, address, phone } = user;
+    const searchFields = [email, company, address, phone].map(field => field.toLowerCase());
+    return searchFields.find(field => field.includes(searchkey)) || '';
+};
+
+const publishUsers = (users) => {
+    const result = document.getElementById("result");
+    const html = users.map(user => createUserHtml(user)).join('');
+    result.innerHTML = html;
+};
+
+const createUserHtml = (user) => {
+    return `
+        <div class='kbu-user-wrap'>
+            <div class='userimg'><img src='http://placehold.it/32x32'></div>
+            <div class='user-desc'> 
+                <span>${user.primary}</span>
+                <p>${user.secondary}</p>
+            </div>
+        </div>
+    `;
+};
+
+const highlightMatched = (content, searchkey) => {
+    if (!content) return '';
+    const regex = new RegExp(`(${searchkey})`, "gi");
+    return content.replace(regex, `<em class="highlight">$1</em>`);
+};
+
+const clearResults = () => {
+    const result = document.getElementById("result");
+    result.innerHTML = "";
+};
